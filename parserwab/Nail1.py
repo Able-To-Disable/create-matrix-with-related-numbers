69df244f8ece0fea3d9d081be1e62a33351544c9
Able <able_n@mail.ru>
2024-03-17 12:11:33 +0300

    Initial commit

---
+++ parserwab/Nail1.py
@@ -0,0 +1,47 @@
+import random
+import pickle
+
+
+class MatrixManager:
+    _defolt_file_name = 'matrix_data.pkl'
+
+    def __init__(self, rows=3, cols=5):
+        self._rows = rows
+        self._cols = cols
+
+    def get_new_matrix(self) -> list[list[int]]:
+        """создаём и заполняем матрицу"""
+        target_sum = 300
+        new_matrix = []
+        for i in range(self._rows):
+            row = []
+            for j in range(self._cols):
+                if j == self._cols - 1:
+                    item = target_sum - sum(row)
+                else:
+                    max_val = min(150, target_sum - sum(row) - (self._cols - j - 1))
+                    item = random.randint(1, max_val)
+                row.append(item)
+            new_matrix.append(row)
+        return new_matrix
+
+    @staticmethod
+    def display_matrix(matrix_to_display):
+        """выводим матрицу"""
+        for row in matrix_to_display:
+            print(row)
+        total_sum = sum([sum(row) for row in matrix_to_display])
+        print("Сумма всех значений в матрице:", total_sum)
+
+    @classmethod
+    def write_matrix(cls, matrix_to_write, file_name=None):
+        """записываем значения в файл"""
+        with open(file_name or cls._defolt_file_name, 'wb') as f:
+            pickle.dump(matrix_to_write, f)
+
+
+if __name__ == "__main__":
+    matrix_manager = MatrixManager()
+    matrix = matrix_manager.get_new_matrix()
+    matrix_manager.display_matrix(matrix)
+    matrix_manager.write_matrix(matrix)
